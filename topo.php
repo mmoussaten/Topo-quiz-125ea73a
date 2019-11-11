b96f8663259b50cfc1013dd20202ce8b7842bb67
marwan <marwan0320@live.nl>
2019-11-11 11:32:32 +0100

    Uitwerking van de exercise

---
+++ topo.php
@@ -0,0 +1,21 @@
+<?php
+
+$antwoord = array(Japan => Tokyo,
+Mexico => MexicoCity,
+USA => Washington,
+India => NewDelhi,
+Zuid-Korea => Seoul,
+China => Peking,
+Nigeria => Abuja,
+Argentina => BuenosAires,
+Egypt => Cairo,
+uk => London,);
+{
+    echo "heb je zin in een leuke topo quizzz :)\n ";
+    $ja = readline();
+    {
+        echo "leuk dat je mee doet laten we beginnen ! \n ";
+
+    }
+    foreach ($antwoord as $landen => $hoofdstad) ; 
+}
